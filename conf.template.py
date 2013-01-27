ab85497f347d7c79f50e322f2125b4603f164535
Colin Stevens <cstev@localhost.localdomain>
2013-01-27 03:07:43 -0600

    Removed

--- conf.template.py
+++
@@ -1,8 +0,0 @@
-#Remember to rename this file to conf.py and change values according to your needs
-#Read the README for a descripton of each variable!
-
-PSCPdir=r'C:\Networking\pscp.exe'
-downloadDir=None
-user='atlas'
-host='example.com'
-waitTime=5
